c54ed72bfc33d081fbc5f4cfa8921e5b2edad663
ingotell <tell.inigo@gmail.com>
2026-08-22 17:05:20 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE='txanda-v2';
+const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(
+    fetch(e.request).then(r=>{
+      if(r.ok && e.request.url.startsWith(self.location.origin)){
+        const cp=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,cp));
+      }
+      return r;
+    }).catch(()=>caches.match(e.request).then(m=>m||caches.match('./index.html')))
+  );
+});

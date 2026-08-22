97fc8b111778f6d6c7fcefc8dd428afd8c1a38bd
ingotell <tell.inigo@gmail.com>
2026-08-22 17:42:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,17 @@
-const CACHE='txanda-v2';
+const CACHE='txanda-v3';
 const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+/* Se cachea archivo a archivo: si uno falla, el resto sigue y el SW se instala igual. */
 self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c=>Promise.all(FILES.map(f=>c.add(f).catch(()=>null))))
+      .then(()=>self.skipWaiting())
+  );
 });
 self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+  e.waitUntil(caches.keys()
+    .then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+    .then(()=>self.clients.claim()));
 });
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET') return;
